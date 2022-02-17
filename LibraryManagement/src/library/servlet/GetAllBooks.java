deb4a6a54faa8638a6c7cfd5a9cba442db9adafd
VinayKumarRudra <42875198+VinayKumarRudra@users.noreply.github.com>
2022-02-17 22:29:33 +0530

    Add files via upload
    done spliting of code

--- LibraryManagement/src/library/servlet/GetAllBooks.java
+++ LibraryManagement/src/library/servlet/GetAllBooks.java
@@ -2,6 +2,7 @@ package library.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,9 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import library.GetBookList;
+import library.LibraryGetAllBook;
 /**
  * Servlet implementation class GetAllBooks
  */
@@ -32,43 +31,18 @@ public class GetAllBooks extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		GetBookList.getBookList();
-		
-		PrintWriter out = response.getWriter();
-		boolean over = false;
-		
-		
 		int pageid = Integer.parseInt(request.getParameter("page"));
 		int per_page = Integer.parseInt(request.getParameter("per_page"));
 		
-		int totalrecords = GetBookList.row_number-1;
-		int totalpages = 0;
-		int pagenumber = pageid;
+		List<String> list = LibraryGetAllBook.getBooks(pageid,per_page);
 		
-		totalpages = totalrecords/per_page;
-		if(totalrecords > totalpages*per_page) {
-			totalpages = totalpages+1;
-		}
+		PrintWriter out = response.getWriter();
 		
-		
-		if(pageid == 1) {
-			
+		if(list.isEmpty()) {
+			response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+		} else {
+			out.println(list);
 		}
-		else {
-			pageid = pageid-1;
-			pageid = pageid*per_page+1;
-		}
-
-		if(pagenumber <= totalpages) {
-			for(int i=pageid-1;i<pageid+per_page-1;i++) {
-				if(i<totalrecords) {
-					out.println(GetBookList.map_book.get(i));
-				}
-			} 
-		} else{
-			 response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-		}
-		
 		
 	}
 
