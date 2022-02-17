371f51d8010cbbc0d597fb8dc53f7acb3491deb7
VinayKumarRudra <42875198+VinayKumarRudra@users.noreply.github.com>
2022-02-17 22:31:09 +0530

    Add files via upload

---
+++ LibraryManagement/src/library/RowNumber.java
@@ -0,0 +1,47 @@
+//$Id$
+package library;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class RowNumber {
+	public static int getBookById(String bookname) throws IOException {
+		Library lib = new Library();
+		int row_number = 0;
+		
+		
+		FileInputStream file = new FileInputStream("/home/local/ZOHOCORP/vinay-pt4139/Downloads/books.xlsx");
+		XSSFWorkbook book = new XSSFWorkbook(file);
+		
+		DataFormatter dataFormatter = new DataFormatter();
+        Iterator<Sheet> sheets = book.sheetIterator();
+        
+        while(sheets.hasNext()) {
+        	Sheet sh = sheets.next();
+        	Iterator<Row> iterator =  sh.iterator();
+        	while(iterator.hasNext()) {
+        		Row row = iterator.next();
+        		Iterator<Cell> cellIterator = row.iterator();
+        		while (cellIterator.hasNext()) {
+        			
+        			Cell cell = cellIterator.next();
+        			String book_name = dataFormatter.formatCellValue(cell);
+        			
+        			if(book_name.equals(bookname)) {
+        				row_number = row.getRowNum();
+        			}
+        		}
+        	}
+        }
+        
+		return row_number;
+        
+	}
+}
