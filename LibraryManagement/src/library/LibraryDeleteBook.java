1a6e8575450c4129d64d52c2a0599b35e463d875
VinayKumarRudra <42875198+VinayKumarRudra@users.noreply.github.com>
2022-03-02 11:31:57 +0530

    Add files via upload

--- LibraryManagement/src/library/LibraryDeleteBook.java
+++ LibraryManagement/src/library/LibraryDeleteBook.java
@@ -4,6 +4,11 @@ package library;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
@@ -12,37 +17,52 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class LibraryDeleteBook {
 
-	public static int deleteBook(String book_id) throws IOException {
+	public static int deleteBook(String book_id) throws IOException, Exception {
 		
-		int rownumber = RowNumber.getBookById(book_id);
+Library lib = new Library();
+		
+		String url = "jdbc:mysql://localhost:3306/sample";
+		String uname = "root";
+		String pass = "vinay";
+		String author_name = "vinay";
+		 
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url,uname,pass);
+		
 		int status = 0;
 		
-		FileInputStream file = new FileInputStream("/home/local/ZOHOCORP/vinay-pt4139/Downloads/book1.xlsx");
-		XSSFWorkbook book = new XSSFWorkbook(file);
-		DataFormatter dataFormatter = new DataFormatter();
-        Sheet sheet = book.getSheetAt(0);
-        
-        int lastRowNum=sheet.getLastRowNum();
-        
-        if(rownumber>=1 && rownumber<lastRowNum) {
-        	sheet.shiftRows(rownumber+1,lastRowNum, -1);
-        	System.out.println("hai");
-        	status =1;
-        }
-        if(rownumber == lastRowNum) {
-        	Row row = sheet.getRow(rownumber);
-        	if(row != null) {
-        		sheet.removeRow(row);
-        		status = 1;
-        		System.out.println("hello");
-        	}
-        	
-        }
-        
-        FileOutputStream outstream = new FileOutputStream("/home/local/ZOHOCORP/vinay-pt4139/Downloads/book1.xlsx");
-		book.write(outstream);
-		outstream.close();
+		String query = "select author_id from temp2 where book_id ='"+book_id+"'";
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
 		
+		if(rs.next()) {
+			
+			int author_id = rs.getInt(1);
+			PreparedStatement ps=con.prepareStatement("delete from temp2 where book_id =?");
+			ps.setString(1,book_id);
+			int val = ps.executeUpdate();
+			status =1;
+			
+			query = "select author_id from temp2 where author_id ='"+author_id+"'";
+			rs = st.executeQuery(query);
+			int count =0;
+			while(rs.next()) {
+				count++;
+			}
+			
+			if(count>0) {
+				
+			} else {
+				ps=con.prepareStatement("delete from temp1 where author_id =?");
+				ps.setInt(1,author_id);
+				val = ps.executeUpdate();
+				status =1;
+			}
+			
+		}else {
+			status = 0;
+		}
+
 		return status;
 	}
 
